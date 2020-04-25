21a33448a477ba1683d9a04aeea2e4c99af72658
zongheng14 <zongheng1314@163.com>
2020-04-25 21:29:49 +0800

    Initial commit

---
+++ src/main/java/com/human/Gof23Application.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gof23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gof23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/human/Gof23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gof23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
